fa8d49f2f90371920cd77ebfc74fe1f3df98ac47
andrei09ctr <88727542+andrei09ctr@users.noreply.github.com>
2021-12-09 14:53:45 +0200

    added mongo DB

--- app.js
+++ app.js
@@ -65,8 +65,8 @@ const requestedPostId = req.params.postId;
 
   Post.findOne({_id: requestedPostId}, function(err, post){
     res.render("post", {
-      title: post.title,
-      content: post.content
+      postTitle: post.title,
+      postContent: post.content
     });
   });
 
